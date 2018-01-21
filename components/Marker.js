c243b2d5a7e390ddee2e4802419816022f6e6551
yonmey <yonmey@gmail.com>
2018-01-21 19:05:12 +0100

    Marker InfoWindow template added

--- components/Marker.js
+++ components/Marker.js
@@ -1,16 +1,24 @@
 import Config from "../config/config";
+const template = require("./InfoWindowTpl.ejs");
 
 export default class Marker {
   createMarker(markerData) {
+    let compiledHTML = template({
+      name: markerData.fields.nom, // todo change specific name
+      address: markerData.fields.adresse, // todo change specific name
+      phone: markerData.fields.tel, // todo change specific name
+      email: markerData.fields.mail // todo change specific name
+    });
+
     let infowindow = new google.maps.InfoWindow({
-      content: "Render HTML here"
+      content: compiledHTML
     });
 
     let marker = new google.maps.Marker({
       icon: Config.googleMaps.markers.iconUrl,
       position: {
-        lat: markerData.geometry.coordinates[1],
-        lng: markerData.geometry.coordinates[0]
+        lat: markerData.geometry.coordinates[1], // todo change specific name
+        lng: markerData.geometry.coordinates[0] // todo change specific name
       },
       map: map
     });
