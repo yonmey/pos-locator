42da09d4d97a6bd79c6fdabc55159e55d6d67988
yonmey <yonmey@gmail.com>
2018-01-21 02:35:55 +0100

    GeoLocator refactored

--- components/GeoLocator.js
+++ components/GeoLocator.js
@@ -1,15 +1,30 @@
-export default class GeoLocator {
-  locate() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        let pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
+import Config from "../config/config";
 
-        map.setCenter(pos);
-        map.setZoom(17);
+export default class GeoLocator {
+  getCurrentPosition() {
+    if (navigator.geolocation) {
+      let positionPromise = () =>
+        new Promise((res, rej) => {
+          navigator.geolocation.getCurrentPosition(res, rej);
+        });
+
+      return positionPromise().then(currentPosition => {
+        let latLntPosition = new google.maps.LatLng({
+          lat: currentPosition.coords.latitude,
+          lng: currentPosition.coords.longitude
+        });
+
+        return latLntPosition;
       });
     }
+  }
+
+  locate() {
+    return this.getCurrentPosition().then(currentPosition => {
+      map.setCenter(currentPosition);
+      map.setZoom(17);
+
+      return currentPosition;
+    });
   }
 }
