9e44acb9eb7c9781603590a87f29736e8fbb855c
yonmey <yonmey@gmail.com>
2018-01-21 02:36:28 +0100

    Distance display added

--- components/Panel.js
+++ components/Panel.js
@@ -1,7 +1,15 @@
 import Config from "../config/config";
+import Markers from "./Markers";
 
 export default class Panel {
   load() {
     const panelElement = document.getElementById(Config.elements.panel);
+  }
+
+  showClosestPOS(position) {
+    const markers = new Markers();
+    let distance = markers.getDistanceToClosestMarker(position);
+
+    console.log("Closest marker at", distance, "meters");
   }
 }
